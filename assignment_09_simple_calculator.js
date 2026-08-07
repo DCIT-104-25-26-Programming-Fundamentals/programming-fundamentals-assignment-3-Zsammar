28b8f4d1c9579dbc1d99ed8e93c9adfccbc0db4a
Zsammar <saahmed006@st.ug.edu.gh>
2026-08-07 17:41:49 +0000

    assignment 9 done.

--- assignment_09_simple_calculator.js
+++ assignment_09_simple_calculator.js
@@ -74,4 +74,110 @@
 // YOUR CODE BELOW — remove the // symbols from the scaffold and fill it in
 // =============================================================================
 
+const readlineSync = require("readline-sync");
 
+function add(a ,b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function modulus(a, b) {
+  return a % b;
+}
+
+function exponentiate(a, b) {
+  return a ** b;
+}
+
+function getTwoNums() {
+  const first = readlineSync.questionFloat("Enter first number: ");
+  const second = readlineSync.questionFloat("Enter second number: ");
+  return [first, second];
+}
+
+function printMenu() {
+  console.log("================================");
+  console.log("        SIMPLE CALCULATOR   ");
+  console.log("================================");
+  console.log("1. Addition");
+  console.log("2. Subtraction");
+  console.log("3. Multiplication");
+  console.log("4. Division");
+  console.log("5. Modulus");
+  console.log("6. Exponentiation");
+  console.log("7. Quit");
+}
+
+
+function runOperation(choice) {
+  const [a, b] = getTwoNums();
+  let result;
+  let symbol;
+
+  if (choice === 1) {
+    result = add(a, b);
+    symbol = "+";
+  }
+  else if (choice === 2) {
+    result = subtract(a, b);
+    symbol = "-";
+  }
+  else if (choice === 3) {
+    result = multiply(a, b);
+    symbol = "*";
+  }
+  else if (choice === 4) {
+    result = divide(a, b);
+    symbol = "/";
+  }
+  else if (choice === 5) {
+    result = modulus(a, b);
+    symbol = "%";
+  }
+  else if (choice === 6) {
+    result = exponentiate(a, b);
+    symbol = "**";
+  }
+
+  if (result === null) {
+    console.log("Error: Cannot divide ny zero.");
+    return;
+  }
+  console.log("Result: " + a + " " + symbol + " " + b + " = " + result.toFixed(2));
+}
+
+/* Main Program */
+function main() {
+  let running =  true;
+
+  while (running) {
+    printMenu();
+    const choice = readlineSync.questionInt("Select an operation 1 - 7: ");
+    console.log("");
+  
+    if (choice >= 1 && choice <= 6) {
+      runOperation(choice);
+    }
+    else if (choice === 7) {
+      console.log("Goodbye!");
+      running = false;
+    }
+    else {
+      console.log("Error: PLease choose a number between 1 and 7.");
+    }
+    console.log("");
+  }
+}
+
+main();
